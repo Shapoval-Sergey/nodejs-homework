1542d53a3cde8bc8f1f1e68693e86aef7a78b06e
Sergey <shapa92sergey@gmail.com>
2020-11-17 00:08:09 +0200

    02-express

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -39,9 +39,64 @@ const getContactById = (req, res, next) => {
     next(error);
   }
 };
-const removeContact = (req, res, next) => {};
-const addContact = (req, res, next) => {};
-const updateContact = (req, res, next) => {};
+const removeContact = (req, res, next) => {
+  try {
+    const contact = contactsService.removeContact(req.params);
+    if (contact) {
+      return res.status(HttpCode.OK).json({
+        status: 'success',
+        code: HttpCode.OK,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      return next({
+        status: HttpCode.NOT_FOUND,
+        message: 'Not found contact',
+        data: 'Not Found',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+const addContact = (req, res, next) => {
+  try {
+    const contact = contactsService.addContact(name, email, phone);
+    res.status(HttpCode.CREATED).json({
+      status: 'success',
+      code: HttpCode.CREATED,
+      data: {
+        contact,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const updateContact = (req, res, next) => {
+  try {
+    const contact = contactsService.updateContact(req.params, req.body);
+    if (contact) {
+      return res.status(HttpCode.OK).json({
+        status: 'success',
+        code: HttpCode.OK,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      return next({
+        status: HttpCode.NOT_FOUND,
+        message: 'Not found contact',
+        data: 'Not Found',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   listContacts,
