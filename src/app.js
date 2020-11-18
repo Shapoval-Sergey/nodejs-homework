6d389c2a287d658fbfaefec51734145c7e34321c
Sergey <shapa92sergey@gmail.com>
2020-11-18 19:09:59 +0200

    fix gitignore

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const app = express();
+const { HttpCode } = require('./helpers/constants');
+const routerContacts = require('./api/contacts');
+
+const isDev = process.env.NODE_ENV === 'development';
+if (isDev) app.use(logger('dev'));
+
+app.use(cors('*'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/contacts', routerContacts);
+
+app.use((req, res, next) => {
+  res.status(HttpCode.NOT_FOUND).json({
+    status: 'error',
+    code: HttpCode.NOT_FOUND,
+    message: `Use api on routes ${req.baseUrl}/api/contacts`,
+    data: 'Not Found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  err.status = err.status ? err.status : HttpCode.INTERNAL_SERVER_ERROR;
+  res.status(err.status).json({
+    status: err.status === 500 ? 'fail' : 'error',
+    code: err.status,
+    message: err.message,
+    data: err.status === 500 ? 'Internal Server Error' : err.data,
+  });
+});
+
+const Port = process.env.PORT || 3000;
+
+app.listen(Port, () => {
+  console.log(`Server running. Use our API on port: ${Port}`);
+});
